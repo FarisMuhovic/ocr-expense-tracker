094eb60452035bc4f543a2fe6cd9aad7b965667d
Faris Muhovic <faris.fare123456@gmail.com>
2024-09-12 11:12:37 +0200

    hash functions

---
+++ api/utils/hashing.js
@@ -0,0 +1,15 @@
+const bcrypt = require("bcryptjs")
+
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10)
+  return await bcrypt.hash(password, salt)
+}
+
+async function comparePasswords(raw, hash) {
+  return await bcrypt.compare(raw, hash)
+}
+
+module.exports = {
+  hashPassword,
+  comparePasswords,
+}
